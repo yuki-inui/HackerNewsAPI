14c5bfbf8756f6fa4c662605b8688964c2457383
yuki-inui <courage8210@gmail.com>
2022-06-12 21:49:13 +0900

    latst

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { statSync } from 'fs'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
@@ -25,7 +26,256 @@ const Home: NextPage = () => {
           FDA urges patch of Illumina devices with three<br>
           critical flaws ranked 10 in severity(scmagazine.com)</br>
         </p>
-        /// div で囲んでグリッド配置を設定
+
+        <section>
+          <p className={styles.any}>
+            Story
+          </p>
+          <p className={styles.any}>
+            <br>Vectorized and performance-</br>
+            <br>portable Quickort</br>
+            (googleblog.com)
+          </p>
+          <p className={styles.any}>
+            by slackerIII 22 hours ago
+          </p>
+        </section>
+
+        <section>
+          <p className={styles.any}>
+            Story
+          </p>
+          <p className={styles.any}>
+          <br>The children of atomic bomb</br>
+          <br>survivors:A genetic study</br>
+          (nih.gov)
+          </p>
+          <p className={styles.any}>
+            by bryarasmussen 22 hours ago
+          </p>
+        </section>
+
+        <section>
+          <p className={styles.any}>
+            Story
+          </p>
+          <p className={styles.any}>
+            <br>Japan tested a giant turbine</br>
+            <br>that generates electoricity using</br>
+            <br>deep ocean current...</br>
+          </p>
+          <p className={styles.any}>
+            by linuxblender 12 minuts ago
+          </p>
+        </section>
+
+        <section>
+          <p className={styles.any}>
+            Story
+          </p>
+          <p className={styles.any}>
+            <br>Question candidates can ask</br>
+            <br>about equity compensation</br>
+            <br>(holloway.com)</br>
+          </p>
+          <p className={styles.any}>
+            by linuxblender 12 minuts ago
+          </p>
+        </section>
+
+        <section>
+          <p className={styles.any}>
+            Story
+          </p>
+          <p className={styles.any}>
+            <br>Painless desktop containers</br>
+            <br>for everyday development</br>
+            <br>(orion3.space)</br>
+          </p>
+          <p className={styles.any}>
+            by linuxblender 12 minuts ago
+          </p>
+        </section>
+
+        <section>
+          <p className={styles.any}>
+            Ask
+          </p>
+          <p className={styles.any}>
+            <br>what is the best jurisdiction</br>
+            <br>for insternationally distributed</br>
+            <br>teams?</br>
+          </p>
+          <p className={styles.any}>
+            by linuxblender 12 minuts ago
+          </p>
+        </section>
+
+        <section>
+          <p className={styles.any}>
+            Story
+          </p>
+          <p className={styles.any}>
+            <br>DDR5 RAM prices crashed by</br>
+            <br>20% in May</br>
+            <br>(tech4gamers.com)</br>
+          </p>
+          <p className={styles.any}>
+            by linuxblender 12 minuts ago
+          </p>
+        </section>
+
+        <section>
+          <p className={styles.any}>
+            Story
+          </p>
+          <p className={styles.any}>
+            <br>Albert - open-source keyboard</br>
+            <br>launcher for linux</br>
+            <br>(albertlauncher.github.io)</br>
+          </p>
+          <p className={styles.any}>
+            by linuxblender 12 minuts ago
+          </p>
+        </section>
+
+        <section>
+          <p className={styles.any}>
+            Story
+          </p>
+          <p className={styles.any}>
+            <br>The power C compiler</br>
+            <br>(mixsoftware.com)</br>
+          </p>
+          <p className={styles.any}>
+            by linuxblender 12 minuts ago
+          </p>
+        </section>
+
+        <section>
+          <p className={styles.any}>
+            Story
+          </p>
+          <p className={styles.any}>
+            <br>What rr does (rr-project.org)</br>
+          </p>
+          <p className={styles.any}>
+            by linuxblender 12 minuts ago
+          </p>
+        </section>
+
+        <section>
+          <p className={styles.any}>
+            Story
+          </p>
+          <p className={styles.any}>
+            <br>UIstein X-BOW inverted bow</br>
+            <br>(ulstein.com)</br>
+          </p>
+          <p className={styles.any}>
+            by linuxblender 12 minuts ago
+          </p>
+        </section>
+
+        <section>
+          <p className={styles.any}>
+            Story
+          </p>
+          <p className={styles.any}>
+            <br>Google alternative that</br>
+            <br>shares revenues with creators -</br>
+            <br>by Ahefs(yep.com)</br>
+          </p>
+          <p className={styles.any}>
+            by linuxblender 12 minuts ago
+          </p>
+        </section>
+
+        <section>
+          <p className={styles.any}>
+            Story
+          </p>
+          <p className={styles.any}>
+            <br>In a small trial, drugs seemed</br>
+            <br>to rejuvenate the body`s</br>
+            <br>`epigenetic clock`(2019)</br>
+          </p>
+          <p className={styles.any}>
+            by linuxblender 12 minuts ago
+          </p>
+        </section>
+
+        <section>
+          <p className={styles.any}>
+            Story
+          </p>
+          <p className={styles.any}>
+            <br>Crabs evolved at least five</br>
+            <br>times from separate group of</br>
+            <br>crustaceans(2021)</br>
+          </p>
+          <p className={styles.any}>
+            by linuxblender 12 minuts ago
+          </p>
+        </section>
+
+        <section>
+          <p className={styles.any}>
+            Story
+          </p>
+          <p className={styles.any}>
+            <br>Pixel Art: Common Mistakes</br>
+            <br>(2020) (derekyu.com)</br>
+          </p>
+          <p className={styles.com}>
+            by linuxblender 12 minuts ago
+          </p>
+        </section>
+
+        <section>
+          <p className={styles.any}>
+            Story
+          </p>
+          <p className={styles.any}>
+            <br>Shown HN: Plasmo -a</br>
+            <br>framework for building modern</br>
+            <br>Chrome extensions...</br>
+          </p>
+          <p className={styles.any}>
+            by linuxblender 12 minuts ago
+          </p>
+        </section>
+
+        <section>
+          <p className={styles.any}>
+            Story
+          </p>
+          <p className={styles.any}>
+            <br>Dear Spotify, can we just get a</br>
+            <br>table of songs?</br>
+            <br>(neil.computer)</br>
+          </p>
+          <p className={styles.any}>
+            by linuxblender 12 minuts ago
+          </p>
+        </section>
+
+        <section>
+          <p className={styles.any}>
+            Story
+          </p>
+          <p className={styles.any}>
+            <br>Software Automatic Mouth-</br>
+            <br>Tiny Speech Synthesizer</br>
+            <br>(github.com/s-macke)</br>
+          </p>
+          <p className={styles.any}>
+            by linuxblender 12 minuts ago
+          </p>
+        </section>
+        <p>More Ask HN </p>
+
+        
       </main>
 
       <footer className={styles.footer}>
